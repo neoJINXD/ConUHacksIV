15f4bc2e3540883e2623e43d055dbff43191c47d
rex884 <46757640+rex884@users.noreply.github.com>
2019-01-26 18:28:54 -0500

    merge with bullet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public GameObject pos1;
     public GameObject bulletLeft;
     public GameObject pos2;
+    public float maxSpeed = 10000.0f;
+
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,5 +30,16 @@ public class PlayerMovement : MonoBehaviour
             GameObject bullet01 = (GameObject)Instantiate(bulletRight);
             bullet01.transform.position = pos1.transform.position;
         }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = new Vector2(moveHorizontal * 5, moveVertical * 5);
+
     }
 }

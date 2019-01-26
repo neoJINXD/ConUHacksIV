381b46d9a5c5bbc5a0854ab80a41161bce29f32f
rex884 <46757640+rex884@users.noreply.github.com>
2019-01-26 18:18:11 -0500

    change test.unity to JohnnyTest.unity

---
+++ Assets/PLayerMovement.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class PLayerMovement : MonoBehaviour
+{
+    public float maxSpeed=10000.0f;
+
+    public Rigidbody2D rb;
+    
+
+    // Update is called once per frame
+    void Start()
+    {
+        rb =GetComponent<Rigidbody2D>();
+       
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = new Vector2(moveHorizontal*5, moveVertical*5);
+
+    }
+
+
+
+    
+}

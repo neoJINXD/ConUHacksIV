dafa275eb6d624b8f1ee9dfe54e681f070f957b0
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2019-01-27 03:12:54 -0500

    Added enemy AI

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+   
+    Vector3 target;
+    Vector2 target2;
+    Transform targetPos;
+    /* int MaxDist = 30;
+    int MinDist = 5;*/
+    int MoveSpeed = 4;
+ 
+    void Start()
+    {
+   
+    }
+ 
+    void Update()
+    {
+        float step = MoveSpeed * Time.deltaTime;
+        targetPos = GameObject.FindWithTag("Player").transform; 
+        var distance = Vector2.Distance(transform.position, targetPos.position);
+        if(distance > 2) {
+        //target the player
+    /*     // get the angle
+        Vector3 norTar = (target-transform.position).normalized;
+        float angle = Mathf.Atan2(norTar.y,norTar.x)*Mathf.Rad2Deg;
+        // rotate to angle
+        Quaternion rotation = new Quaternion ();
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); */
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, targetPos.position, step);
+        }
+    }
+ }
---
+++ Assets/Scripts/EnemyMovementChase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementChase : MonoBehaviour
+{
+   
+    Vector3 target;
+    Vector2 target2;
+    Transform targetPos;
+    int MoveSpeed = 4;
+ 
+    void Start()
+    {
+   
+    }
+ 
+    void Update()
+    {
+        float step = MoveSpeed * Time.deltaTime;
+        //target the player
+        targetPos = GameObject.FindWithTag("Player").transform; 
+        var distance = Vector2.Distance(transform.position, targetPos.position);
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, targetPos.position, step);
+    }
+ }
+

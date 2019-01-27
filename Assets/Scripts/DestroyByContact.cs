915291244ff1b052966eb9d4723d2639701f82c2
neo <anik_patel@live.com>
2019-01-27 04:45:56 -0500

    Added game over condition and reset/quit

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+    
     void OnTriggerEnter2D(Collider2D other) 
     {
         Destroy(other.gameObject);
         Destroy(gameObject);
+        FindObjectOfType<gameManager>().gameOver();
     }
 }
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    bool gameIsOver = false;
+    public void gameOver()
+    {
+        if (gameIsOver == false)
+        {
+            gameIsOver = true;
+            Debug.Log("GAMEOVER!");
+            gameOverScreen();
+        }
+       
+    }
+
+    void gameOverScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+}

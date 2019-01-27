de203c7f7053dcddbbdef14a176280c1f3ae746a
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2019-01-27 10:42:22 -0500

    Add health counter to Lord Gaben

---
+++ Assets/Scripts/BoxHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxHealth : MonoBehaviour
+{
+    int killedCounter=0;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Destroy(other.gameObject);
+        killedCounter++;
+
+        if(killedCounter == 100)
+            Destroy(gameObject);
+    }
+
+    
+
+   
+
+}

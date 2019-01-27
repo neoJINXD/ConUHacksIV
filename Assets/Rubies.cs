7b3b213aefc19a47214476ea25726bb3f7c4847a
rex884 <46757640+rex884@users.noreply.github.com>
2019-01-27 05:02:48 -0500

    e

---
+++ Assets/Rubies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rubies : MonoBehaviour
+{
+    public int health;
+    public int numOfRubies;
+
+    public Sprite[] rubies;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < rubies.Length; i++)
+        {
+
+        }
+    }
+}
